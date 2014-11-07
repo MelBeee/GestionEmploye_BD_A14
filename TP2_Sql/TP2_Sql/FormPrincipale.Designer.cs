97fcb0a1f9dd1eef0ac69eed08c0f48889b6513f
MelBeee <mel.beee@deepteal.com>
2014-11-06 22:10:10 -0500

    Ajout Ressource/ Changement formprincipale

--- TP2_Sql/TP2_Sql/FormPrincipale.Designer.cs
+++ TP2_Sql/TP2_Sql/FormPrincipale.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Connexion = new System.Windows.Forms.Button();
+            this.btn_Deconnexion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Ajouter = new System.Windows.Forms.Button();
+            this.btn_Supprimer = new System.Windows.Forms.Button();
+            this.btn_Modifier = new System.Windows.Forms.Button();
+            this.btn_Rechercher = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btn_Connexion
+            // 
+            this.btn_Connexion.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_Connexion.Location = new System.Drawing.Point(71, 19);
+            this.btn_Connexion.Name = "btn_Connexion";
+            this.btn_Connexion.Size = new System.Drawing.Size(99, 33);
+            this.btn_Connexion.TabIndex = 0;
+            this.btn_Connexion.Text = "Connexion";
+            this.btn_Connexion.UseVisualStyleBackColor = true;
+            // 
+            // btn_Deconnexion
+            // 
+            this.btn_Deconnexion.Location = new System.Drawing.Point(176, 19);
+            this.btn_Deconnexion.Name = "btn_Deconnexion";
+            this.btn_Deconnexion.Size = new System.Drawing.Size(99, 33);
+            this.btn_Deconnexion.TabIndex = 1;
+            this.btn_Deconnexion.Text = "Deconnexion";
+            this.btn_Deconnexion.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TP2_Sql.Properties.Resources.Deconnecter;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Ajouter
+            // 
+            this.btn_Ajouter.Location = new System.Drawing.Point(71, 147);
+            this.btn_Ajouter.Name = "btn_Ajouter";
+            this.btn_Ajouter.Size = new System.Drawing.Size(99, 33);
+            this.btn_Ajouter.TabIndex = 3;
+            this.btn_Ajouter.Text = "Ajouter";
+            this.btn_Ajouter.UseVisualStyleBackColor = true;
+            // 
+            // btn_Supprimer
+            // 
+            this.btn_Supprimer.Location = new System.Drawing.Point(71, 225);
+            this.btn_Supprimer.Name = "btn_Supprimer";
+            this.btn_Supprimer.Size = new System.Drawing.Size(99, 33);
+            this.btn_Supprimer.TabIndex = 4;
+            this.btn_Supprimer.Text = "Supprimer";
+            this.btn_Supprimer.UseVisualStyleBackColor = true;
+            // 
+            // btn_Modifier
+            // 
+            this.btn_Modifier.Location = new System.Drawing.Point(71, 186);
+            this.btn_Modifier.Name = "btn_Modifier";
+            this.btn_Modifier.Size = new System.Drawing.Size(99, 33);
+            this.btn_Modifier.TabIndex = 5;
+            this.btn_Modifier.Text = "Modifier";
+            this.btn_Modifier.UseVisualStyleBackColor = true;
+            // 
+            // btn_Rechercher
+            // 
+            this.btn_Rechercher.Location = new System.Drawing.Point(71, 314);
+            this.btn_Rechercher.Name = "btn_Rechercher";
+            this.btn_Rechercher.Size = new System.Drawing.Size(99, 33);
+            this.btn_Rechercher.TabIndex = 6;
+            this.btn_Rechercher.Text = "Rechercher";
+            this.btn_Rechercher.UseVisualStyleBackColor = true;
             // 
             // FormPrincipale
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(237)))), ((int)(((byte)(137)))));
-            this.ClientSize = new System.Drawing.Size(1142, 621);
+            this.ClientSize = new System.Drawing.Size(838, 621);
+            this.Controls.Add(this.btn_Rechercher);
+            this.Controls.Add(this.btn_Modifier);
+            this.Controls.Add(this.btn_Supprimer);
+            this.Controls.Add(this.btn_Ajouter);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Deconnexion);
+            this.Controls.Add(this.btn_Connexion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FormPrincipale";
             this.Text = "FormPrincipale";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Connexion;
+        private System.Windows.Forms.Button btn_Deconnexion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Ajouter;
+        private System.Windows.Forms.Button btn_Supprimer;
+        private System.Windows.Forms.Button btn_Modifier;
+        private System.Windows.Forms.Button btn_Rechercher;
     }
 }
