4fab70836dd30c4ef6ebbe1b5f9deedc9915bca8
MelBeee <mel.beee@deepteal.com>
2014-11-06 21:24:13 -0500

    Ajout contraintes check sql
    numero employé doit etre plus grand que 0
    numero echelon aussi
    salaire entre 25000 et 100000 inclusivement

--- TP2_tableaux_sqldeveloper.sql
+++ TP2_tableaux_sqldeveloper.sql
@@ -15,7 +15,10 @@ Prenom varchar2(30),
 Salaire Number(8,2),
 Echelon Number(2,0),
 CodeDep Char(4),
-constraint fkcodedep foreign key(CodeDep) REFERENCES Departements(CodeDep)
+constraint fkcodedep foreign key(CodeDep) REFERENCES Departements(CodeDep),
+constraint ckempno check (empno > 0),
+constraint cksalaire check (salaire >= 25000 and salaire <= 100000),
+constraint ckechelon check (echelon > 0)
 );
 
 INSERT INTO Departements VALUES('100', 'Ressources Humaines');
@@ -24,18 +27,20 @@ INSERT INTO Departements VALUES('300', 'Informatique');
 INSERT INTO Departements VALUES('400', 'Administation');
 INSERT INTO Departements VALUES('500', 'Comptabilité');
 
-/-- Departement 100
+-- Departement 100
 INSERT INTO Employes VALUES(10,'Bourssio','Xavios',100000,1,'100');
 INSERT INTO Employes VALUES(11,'Of CSharp','Queen',100000,2,'100');
-/-- Departement 200
+-- Departement 200
 INSERT INTO Employes VALUES(12,'Baccon','Fredéric',45000,5,'200');
 INSERT INTO Employes VALUES(13,'LemaytoileDeMer','Patrick',78000,4,'200');
-/-- Departement 300
+-- Departement 300
 INSERT INTO Employes VALUES(14,'LeMayor','Roux', 25000,6,'300');
 INSERT INTO Employes VALUES(15,'KohPer','Shoun',65000,7,'300');
-/-- Departement 400
+-- Departement 400
 INSERT INTO Employes VALUES(16,'Deloin','Manuwel',98000,8,'400');
 INSERT INTO Employes VALUES(17,'Syvestio','Davyd', 25000,9,'400');
-/-- Departement 500
+-- Departement 500
 INSERT INTO Employes VALUES(18,'Clémentine','Dominique',90000,3,'500');
 INSERT INTO Employes VALUES(19,'Enfant','Alexiss',50000,10,'500');
+
+commit;
