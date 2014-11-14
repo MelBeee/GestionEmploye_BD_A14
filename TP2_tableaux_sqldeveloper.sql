4158101997f61d6da673080b2d1931b03cf16ca1
MelBeee <mel.beee@deepteal.com>
2014-11-14 15:58:52 -0500

    changement sql

--- TP2_tableaux_sqldeveloper.sql
+++ TP2_tableaux_sqldeveloper.sql
@@ -3,23 +3,23 @@ drop table employes cascade constraints;
 
 create table Departements
 (
-CodeDep Char(4) primary key,
-NomDepartement varchar2(50)
+  CodeDep Char(4) primary key,
+  NomDepartement varchar2(50)
 );
 
 create table Employes
 (
-Empno number(6,0) primary key,
-Nom varchar2(30),
-Prenom varchar2(30),
-Salaire Number(8,2),
-Echelon Number(2,0),
-Adresse varchar2(60),
-CodeDep Char(4),
-constraint fkcodedep foreign key(CodeDep) REFERENCES Departements(CodeDep),
-constraint ckempno check (empno > 0),
-constraint cksalaire check (salaire >= 25000 and salaire <= 100000),
-constraint ckechelon check (echelon > 0)
+  Empno number(6,0) primary key,
+  Nom varchar2(30),
+  Prenom varchar2(30),
+  Salaire Number(8,2),
+  Echelon Number(2,0),
+  Adresse varchar2(60),
+  CodeDep Char(4),
+  constraint fkcodedep foreign key(CodeDep) REFERENCES Departements(CodeDep),
+  constraint ckempno check (empno > 0),
+  constraint cksalaire check (salaire >= 25000 and salaire <= 100000),
+  constraint ckechelon check (echelon > 0)
 );
 
 INSERT INTO Departements VALUES('100', 'Ressources Humaines');
@@ -28,6 +28,7 @@ INSERT INTO Departements VALUES('300', 'Informatique');
 INSERT INTO Departements VALUES('400', 'Administation');
 INSERT INTO Departements VALUES('500', 'Comptabilité');
 
+/*
 -- Departement 100
 INSERT INTO Employes VALUES(10,'Bourssio',        'Xavios',   100000, 1,  '110 Dorion, St-Eustache',    '100');
 INSERT INTO Employes VALUES(11,'Of CSharp',       'Queen',    100000, 2,  '120 Vaillant, Terrebonne',   '100');
@@ -43,5 +44,9 @@ INSERT INTO Employes VALUES(17,'Syvestio',        'Davyd',    25000,  9,  '180 V
 -- Departement 500
 INSERT INTO Employes VALUES(18,'Clémentine',      'Dominique',90000,  3,  '190 Beriot, Boisbriand',     '500');
 INSERT INTO Employes VALUES(19,'Enfant',          'Alexiss',  50000,  10, '200 Delage, Mont-Tremblant', '500');
+*/
+//--- UPDATE EMPLOYES SET ADRESSE = 'BLABLA,   65RTU', ECHELON = 1, SALAIRE = 25000 WHERE EMPNO = 1;
+
+SELECT * FROM EMPLOYES; 
 
 commit;
